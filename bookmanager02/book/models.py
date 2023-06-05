e17cd04b0d6ec820d30d022e62e092918ea9ca07
admin <1652488970@qq.com>
2023-06-05 13:10:34 +0800

    添加数据库信息

--- bookmanager02/book/models.py
+++ bookmanager02/book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=10,unique=True)
+    pub_data = models.DateField(null=True)
+    read_count = models.SmallIntegerField(default=0)
+    commentcount = models.SmallIntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+class People(models.Model):
+    pass
--- bookmanager02/bookmanager02/settings.py
+++ bookmanager02/bookmanager02/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.version_info = (1, 4, 13, "final", 0)
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'bookmanager02.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root@123',
+        'NAME': 'book_42_01',
     }
 }
 
